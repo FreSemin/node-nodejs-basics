98160f23c4f6bfbb7ce6ba8acff2be600f91b9fa
fresemin <fresemin@gmail.com>
2024-01-29 17:52:26 +0300

    ref: fs create use operation failed error util

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,15 +1,16 @@
 import { writeFile } from 'node:fs/promises';
-
-const fileName = 'fresh.txt';
-const filePath = `${process.cwd()}/src/fs/files/${fileName}`;
-const fileContent = 'I am fresh and young';
+import FsOperationFailedError from '../utils/fs-operation-failed.error.js';
 
 const create = async () => {
+    const fileName = 'fresh.txt';
+    const filePath = `${process.cwd()}/src/fs/files/${fileName}`;
+    const fileContent = 'I am fresh and young';
+
     await writeFile(filePath, fileContent, { flag: 'wx' })
         .then(() => {
             console.log('File successfully created!');
         }).catch(() => {
-            console.log('FS operation failed');
+            throw new FsOperationFailedError();
         });
 };
 
