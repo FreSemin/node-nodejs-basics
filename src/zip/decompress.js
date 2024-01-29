8e8d65b2f13d54817301c53d0c9e6c8ab3f8765b
fresemin <fresemin@gmail.com>
2024-01-30 00:49:18 +0300

    feat: zip decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+import { getDirNameFromUrl } from '../utils/path.utils.js';
+import * as path from 'path';
+
+const scriptDir = getDirNameFromUrl(import.meta.url);
+const archiveForUnzip = path.join(scriptDir, 'files', 'archive.gz',);
+const fileDestination = path.join(scriptDir, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        const unzip = createUnzip();
+
+        const archiveSource = createReadStream(archiveForUnzip);
+
+        const fileDestinationStream = createWriteStream(fileDestination);
+
+        archiveSource.pipe(unzip).pipe(fileDestinationStream);
+    } catch (error) {
+        throw error;
+    }
 };
 
 await decompress();
