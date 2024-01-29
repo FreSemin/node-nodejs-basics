75ba49f9b6438058ae77baeb143fdd187c87c110
fresemin <fresemin@gmail.com>
2024-01-29 15:31:40 +0300

    feat: fs create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { writeFile } from 'node:fs/promises';
+
+const fileName = 'fresh.txt';
+const filePath = `${process.cwd()}/src/fs/files/${fileName}`;
+const fileContent = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+    await writeFile(filePath, fileContent, { flag: 'wx' })
+        .then(() => {
+            console.log('File successfully created!');
+        }).catch(() => {
+            console.log('FS operation failed');
+        });
 };
 
 await create();
