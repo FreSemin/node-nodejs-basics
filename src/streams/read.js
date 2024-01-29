0303904a494f96a04261c18b355e39e03e23328d
fresemin <fresemin@gmail.com>
2024-01-30 00:01:03 +0300

    ref: streams read using pipe

--- src/streams/read.js
+++ src/streams/read.js
@@ -3,18 +3,16 @@ import { getDirNameFromUrl } from '../utils/path.utils.js';
 import * as path from 'path';
 
 const scriptDir = getDirNameFromUrl(import.meta.url);
-const FileForRead = path.join(scriptDir, 'files', 'fileToRead.txt');
+const fileForRead = path.join(scriptDir, 'files', 'fileToRead.txt');
 
 const read = async () => {
-    const fileStream = createReadStream(FileForRead, { encoding: 'UTF-8', });
+    try {
+        const fileStream = createReadStream(fileForRead, { encoding: 'UTF-8', });
 
-    fileStream.on('data', (chunk) => {
-        process.stdout.write(chunk);
-    });
-
-    fileStream.on('error', (error) => {
+        fileStream.pipe(process.stdout);
+    } catch (error) {
         throw error;
-    });
+    }
 };
 
 await read();
