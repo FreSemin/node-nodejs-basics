989c7cea1fcc237e6ac2918cc3a480b3333f6cd5
fresemin <fresemin@gmail.com>
2024-01-29 16:38:37 +0300

    feat: fs copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import { readdir, mkdir, copyFile } from 'node:fs/promises';
+
+const folderCopyName = 'files_copy';
+const folderName = 'files';
+const folderPath = `${process.cwd()}/src/fs/`;
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const copyDir = await mkdir(folderPath + folderCopyName);
+
+        const filesToCopy = await readdir(folderPath + folderName)
+            .then((files) => {
+                return files.map((file) => {
+                    return copyFile(`${folderPath}${folderName}/${file}`, `${folderPath}${folderCopyName}/${file}`);
+                });
+            });
+
+        await Promise.all(filesToCopy)
+            .then(() => {
+                console.log('Folder and files has successfully copied!');
+            });
+    } catch {
+        console.log('FS operation failed');
+    }
 };
 
 await copy();
