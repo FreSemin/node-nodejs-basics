a71a5589580a84e399afa7257c80d0f037803622
fresemin <fresemin@gmail.com>
2024-01-29 19:54:10 +0300

    feat: fs read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from 'node:fs/promises';
+import FsOperationFailedError from '../utils/fs-operation-failed.error.js';
+import { getDirNameFromUrl } from '../utils/path.utils.js';
+import * as path from 'path';
+
+const scriptDir = getDirNameFromUrl(import.meta.url);
+const fileToRead = path.join(scriptDir, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    await readFile(fileToRead, { encoding: 'utf8' })
+        .then((content) => {
+            console.log(content);
+        })
+        .catch(() => {
+            throw new FsOperationFailedError();
+        });
 };
 
 await read();
