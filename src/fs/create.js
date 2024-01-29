1b63e123b3aeae5051ed8e9e17e5a7b4f2b6b90d
fresemin <fresemin@gmail.com>
2024-01-29 18:17:26 +0300

    ref: fs create using path util

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,12 +1,15 @@
 import { writeFile } from 'node:fs/promises';
 import FsOperationFailedError from '../utils/fs-operation-failed.error.js';
+import { getDirNameFromUrl } from '../utils/path.utils.js';
+import * as path from 'path';
+
+const fileName = 'fresh.txt';
+const destinationDir = 'files';
+const fileContent = 'I am fresh and young';
+const scriptDir = getDirNameFromUrl(import.meta.url);
 
 const create = async () => {
-    const fileName = 'fresh.txt';
-    const filePath = `${process.cwd()}/src/fs/files/${fileName}`;
-    const fileContent = 'I am fresh and young';
-
-    await writeFile(filePath, fileContent, { flag: 'wx' })
+    await writeFile(path.join(scriptDir, destinationDir, fileName), fileContent, { flag: 'wx' })
         .then(() => {
             console.log('File successfully created!');
         }).catch(() => {
