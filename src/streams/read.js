11489fb21250a3e655c45fc0a08a1418dd51e663
fresemin <fresemin@gmail.com>
2024-01-29 23:39:20 +0300

    feat: streams read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'node:fs';
+import { getDirNameFromUrl } from '../utils/path.utils.js';
+import * as path from 'path';
+
+const scriptDir = getDirNameFromUrl(import.meta.url);
+const FileForRead = path.join(scriptDir, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const fileStream = createReadStream(FileForRead, { encoding: 'UTF-8', });
+
+    fileStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    fileStream.on('error', (error) => {
+        throw error;
+    });
 };
 
 await read();
