00584cc44011aaf89a44f2587a4c20cb516e2726
fresemin <fresemin@gmail.com>
2024-01-29 21:01:12 +0300

    feat: cli args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.splice(2);
+    const resArr = [];
+
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].startsWith('--')) {
+            resArr.push(`${args[i].slice(2)} is ${args[i + 1]}`);
+        }
+    }
+
+    console.log(resArr.join(', '));
 };
 
 parseArgs();
