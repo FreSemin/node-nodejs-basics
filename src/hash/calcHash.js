7bf86d007d38693e9170b5eca2c472bea59de5c9
fresemin <fresemin@gmail.com>
2024-01-29 23:30:33 +0300

    feat: hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+import { getDirNameFromUrl } from '../utils/path.utils.js';
+import * as path from 'path';
+
+const scriptDir = getDirNameFromUrl(import.meta.url);
+const fileForHash = path.join(scriptDir, 'files', 'fileToCalculateHashFor.txt');
+
+const hash = createHash('sha256');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileStream = createReadStream(fileForHash, { encoding: 'UTF-8', });
+
+    fileStream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    fileStream.on('end', () => {
+        console.log(`SHA256 ${hash.digest('hex')}`);
+    });
+
+    fileStream.on('error', (error) => {
+        throw error;
+    });
 };
 
 await calculateHash();
